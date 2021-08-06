1a644a64f7a57b0cedd13668ba4629d080b761c8
ignj <ignacio.jocano@gmail.com>
2021-08-05 21:04:30 -0300

    move type configuration

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
+using HotChocolate;
+using HotChocolate.Execution.Configuration;
+using HotChocolate.Types.Relay;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace hotchocolate_playground
 {
@@ -15,7 +19,7 @@ namespace hotchocolate_playground
         }
 
         public IConfiguration Configuration { get; }
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -25,7 +29,8 @@ namespace hotchocolate_playground
                 .AddGraphQLServer()
                 .AddQueryType(d => d.Name("Query"))
                 .AddTypeExtension<ExampleQueries>()
-                .AddType<ExampleType>()
+                .AddIdSerializer(s => new CustomIdSerializer())
+                .ConfigureTypes()
                 .EnableRelaySupport()
                 .AddDataLoader<ExampleByIdDataLoader>();
         }
@@ -47,5 +52,32 @@ namespace hotchocolate_playground
                 endpoints.MapGraphQL();
             });
         }
+    }
+
+    public class CustomIdSerializer : IIdSerializer
+    {
+        public string Serialize<T>(NameString schemaName, NameString typeName, T id)
+        {
+            return JsonConvert.SerializeObject(id, Formatting.Indented, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.All
+            });
+        }
+
+        public IdValue Deserialize(string serializedId)
+        {
+            var result = JsonConvert.DeserializeObject(serializedId, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto
+            });
+
+            return new IdValue(null, nameof(CompositeKey), result);
+        }
+    }
+
+    public static class TypesConfiguration
+    {
+        public static IRequestExecutorBuilder ConfigureTypes(this IRequestExecutorBuilder builder)
+            => builder.AddType<ExampleType>();
     }
 }
